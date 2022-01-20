862d233a91af028675cc10eecfa4170641dcf6ac
Marcos Kuyoc <marcos.kuyoc@gulf.mx>
2022-01-20 11:08:19 -0600

    Se realiza el primer comit agregando un modelo, un datasource, un repositorio para la creacion de usuarios

---
+++ src/repositories/users.repository.ts
@@ -0,0 +1,16 @@
+import {inject} from '@loopback/core';
+import {DefaultCrudRepository} from '@loopback/repository';
+import {TestDataSource} from '../datasources';
+import {Users, UsersRelations} from '../models';
+
+export class UsersRepository extends DefaultCrudRepository<
+  Users,
+  typeof Users.prototype.id,
+  UsersRelations
+> {
+  constructor(
+    @inject('datasources.test') dataSource: TestDataSource,
+  ) {
+    super(Users, dataSource);
+  }
+}
